c0dc56bef8135e7591da73d8eff1086f09602e41
furfurylic <17105140+furfurylic@users.noreply.github.com>
2022-02-09 19:48:00 +0900

    Refactor sanitize_buffer_size

--- include/commata/buffer_size.hpp
+++ include/commata/buffer_size.hpp
@@ -17,16 +17,14 @@ template <class Allocator>
 std::size_t sanitize_buffer_size(
     std::size_t buffer_size, Allocator alloc) noexcept
 {
-    constexpr std::size_t buffer_size_max =
-        std::numeric_limits<std::size_t>::max();
-    constexpr std::size_t default_buffer_size =
-        std::min(buffer_size_max, static_cast<std::size_t>(8192U));
+    constexpr auto buffer_size_max = std::numeric_limits<std::size_t>::max();
     if (buffer_size == 0U) {
+        constexpr auto default_buffer_size =
+            std::min(buffer_size_max, static_cast<std::size_t>(8192U));
         buffer_size = default_buffer_size;
     }
     const auto max_alloc0 = std::allocator_traits<Allocator>::max_size(alloc);
-    const auto max_alloc = (max_alloc0 > buffer_size_max) ?
-        static_cast<std::size_t>(buffer_size_max) : max_alloc0;
+    const auto max_alloc = std::min(buffer_size_max, max_alloc0);
     return std::min(buffer_size, max_alloc);
 }
 
