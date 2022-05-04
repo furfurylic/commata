14e09ed434f522ffb279698b3c33c96c4c717a60
furfurylic <17105140+furfurylic@users.noreply.github.com>
2022-05-04 21:13:25 +0900

    Add tests for make_char_input
    which have been not tested in terms of their runtime behaviour
    (but statically tested in terms of their return types
    through make_csv_source)

--- src_test/TestCharInput.cpp
+++ src_test/TestCharInput.cpp
@@ -17,6 +17,7 @@
 #include "BaseTest.hpp"
 
 using namespace std::literals::string_literals;
+using namespace std::literals::string_view_literals;
 
 using namespace commata;
 using namespace commata::test;
@@ -45,6 +46,88 @@ TEST_F(TestCharInput, OwnedStringCopy)
         ASSERT_EQ(3U, len);
         ASSERT_EQ("ABCABC", std::string(b.data(), 6));
     }
+}
+
+TEST_F(TestCharInput, MakeFromStreambufPtr)
+{
+    std::wstringbuf buf(L"XYZ");
+    streambuf_input<wchar_t> in = make_char_input(&buf);
+    std::wstring out(5, L' ');
+    ASSERT_EQ(3, in(out.data(), 5));
+    ASSERT_EQ(L"XYZ  ", out);
+}
+
+TEST_F(TestCharInput, MakeFromIStreamLvalueRef)
+{
+    std::istringstream buf("XYZ");
+    streambuf_input<char> in = make_char_input(buf);
+    std::string out(5, ' ');
+    ASSERT_EQ(3, in(out.data(), 5));
+    ASSERT_EQ("XYZ  ", out);
+}
+
+TEST_F(TestCharInput, MakeFromStreambufRvalueRef)
+{
+    std::stringbuf buf("XYZ");
+    owned_streambuf_input<std::stringbuf> in = make_char_input(std::move(buf));
+    std::string out(5, ' ');
+    ASSERT_EQ(3, in(out.data(), 5));
+    ASSERT_EQ("XYZ  ", out);
+}
+
+TEST_F(TestCharInput, MakeFromIStreamRvalueRef)
+{
+    std::istringstream buf("XYZ");
+    owned_istream_input<std::istringstream> in =
+        make_char_input(std::move(buf));
+    std::string out(5, ' ');
+    ASSERT_EQ(3, in(out.data(), 5));
+    ASSERT_EQ("XYZ  ", out);
+}
+
+TEST_F(TestCharInput, MakeFromCharPtr)
+{
+    const wchar_t* const str= L"XYZ";
+    string_input<wchar_t> in = make_char_input(str);
+    std::wstring out(5, L' ');
+    ASSERT_EQ(3, in(out.data(), 5));
+    ASSERT_EQ(L"XYZ  ", out);
+}
+
+TEST_F(TestCharInput, MakeFromCharPtrAndSize)
+{
+    const wchar_t* const str= L"XYZABC";
+    string_input<wchar_t> in = make_char_input(str, 4);
+    std::wstring out(5, L' ');
+    ASSERT_EQ(4, in(out.data(), 4));
+    ASSERT_EQ(L"XYZA ", out);
+}
+
+TEST_F(TestCharInput, MakeFromStringView)
+{
+    const auto str= L"XYZABC"sv;
+    string_input<wchar_t> in = make_char_input(str.substr(2));
+    std::wstring out(5, L' ');
+    ASSERT_EQ(4, in(out.data(), 4));
+    ASSERT_EQ(L"ZABC ", out);
+}
+
+TEST_F(TestCharInput, MakeFromStringLvalueRef)
+{
+    std::wstring str(L"XYZ");
+    string_input<wchar_t> in = make_char_input(str);
+    std::wstring out(5, L' ');
+    ASSERT_EQ(3, in(out.data(), 5));
+    ASSERT_EQ(L"XYZ  ", out);
+}
+
+TEST_F(TestCharInput, MakeFromStringRvalueRef)
+{
+    std::string str("XYZ");
+    owned_string_input<char> in = make_char_input(std::move(str));
+    std::string out(5, ' ');
+    ASSERT_EQ(3, in(out.data(), 5));
+    ASSERT_EQ("XYZ  ", out);
 }
 
 }
