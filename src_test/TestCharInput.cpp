660cf40209ad26dbb70e62c1c8e4a4ad4902887a
furfurylic <17105140+furfurylic@users.noreply.github.com>
2023-02-04 15:56:19 +0900

    Fortify tests for owned_string_input

--- src_test/TestCharInput.cpp
+++ src_test/TestCharInput.cpp
@@ -24,6 +24,87 @@ using namespace commata::test;
 
 namespace {
 
+struct TestOwnedStringInput : BaseTest
+{};
+
+TEST_F(TestOwnedStringInput, ConstructFromStringRValue)
+{
+    std::string s;
+    for (std::size_t i = 0; i < 20; ++i) {
+        s += std::to_string(i);     // inhibit short string optimization
+    }
+    const auto sd = s.data();
+
+    owned_string_input<char> p(std::move(s));
+    std::vector<char> a1(3);
+    std::vector<char> a2(2);
+    const auto len1 = p(a1.data(), 3);
+    const auto len2 = p(a2.data(), 2);
+
+    ASSERT_NE(s.data(), sd);        // sd is moved-from
+    ASSERT_EQ(3U, len1);
+    ASSERT_EQ("012", std::string(a1.data(), 3));
+    ASSERT_EQ(2U, len2);
+    ASSERT_EQ("34", std::string(a2.data(), 2));
+}
+
+TEST_F(TestOwnedStringInput, MoveConstruct)
+{
+    auto p = std::make_unique<owned_string_input<char>>("ABC"s);
+
+    std::vector<char> a(2);
+    std::vector<char> b(3);
+    const auto lenp = (*p)(a.data(), 2);
+    auto q = std::move(*p);
+    p.reset();
+    const auto lenq = q(b.data(), 3);
+
+    ASSERT_EQ(2U, lenp);
+    ASSERT_EQ("AB", std::string(a.data(), 2));
+    ASSERT_EQ(1U, lenq);
+    ASSERT_EQ("C", std::string(b.data(), 1));
+}
+
+TEST_F(TestOwnedStringInput, MoveAssign)
+{
+    auto p = std::make_unique<owned_string_input<char>>("ABC"s);
+    owned_string_input q("XYZ"s);
+
+    std::vector<char> a1(1);
+    std::vector<char> b1(2);
+    std::vector<char> b2(2);
+    (*p)(a1.data(), 1);
+    q(b1.data(), 2);
+    q = std::move(*p);
+    p.reset();
+    const auto lenq2 = q(b2.data(), 2);
+
+    ASSERT_EQ(2U, lenq2);
+    ASSERT_EQ("BC", std::string(b2.data(), 2));
+}
+
+TEST_F(TestOwnedStringInput, Swap)
+{
+    owned_string_input p(L"ABC"s);
+    owned_string_input q(L"XYZ"s);
+
+    std::vector<wchar_t> a1(1);
+    std::vector<wchar_t> a2(1);
+    std::vector<wchar_t> b1(2);
+    std::vector<wchar_t> b2(2);
+    p(a1.data(), 1);
+    q(b1.data(), 2);
+    using std::swap;
+    swap(p, q);
+    const auto lenp2 = p(a2.data(), 1);
+    const auto lenq2 = q(b2.data(), 2);
+
+    ASSERT_EQ(1U, lenp2);
+    ASSERT_EQ(L"Z", std::wstring(a2.data(), 1));
+    ASSERT_EQ(2U, lenq2);
+    ASSERT_EQ(L"BC", std::wstring(b2.data(), 2));
+}
+
 struct TestCharInput : BaseTest
 {};
 
