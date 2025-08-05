9bacb1125a04a297acccfb31a535b26c624e88d6
furfurylic <17105140+furfurylic@users.noreply.github.com>
2025-08-05 20:09:55 +0900

    WIP: optionality

--- include/commata/record_translator.hpp
+++ include/commata/record_translator.hpp
@@ -50,44 +50,48 @@ constexpr auto transform(F f, Tuples&&... ts) {
 
 }
 
-template <class T>
+template <class T,
+    class SkippingHandler = fail_if_skipped,
+    class ConversionErrorHandler = fail_if_conversion_failed>
 struct arithmetic_field_translator_factory
 {
     using target_type = T;
 
     template <class Sink>
-    arithmetic_field_translator<T, std::decay_t<Sink>> operator()(Sink&& sink)
-        const
+    arithmetic_field_translator<T, std::decay_t<Sink>,
+        SkippingHandler, ConversionErrorHandler> operator()(Sink&& sink) const
     {
-        return arithmetic_field_translator<T, std::decay_t<Sink>>(
+        return arithmetic_field_translator<T, std::decay_t<Sink>, SkippingHandler, ConversionErrorHandler>(
             std::forward<Sink>(sink));
     }
 };
 
-template <class Ch, class Tr>
+template <class Ch, class Tr,
+    class SkippingHandler = fail_if_skipped>
 struct string_view_field_translator_factory
 {
     using target_type = std::basic_string_view<Ch, Tr>;
 
     template <class Sink>
-    string_view_field_translator<std::decay_t<Sink>, Ch, Tr>
+    string_view_field_translator<std::decay_t<Sink>, Ch, Tr, SkippingHandler>
         operator()(Sink&& sink) const
     {
-        return string_view_field_translator<std::decay_t<Sink>, Ch, Tr>(
+        return string_view_field_translator<std::decay_t<Sink>, Ch, Tr, SkippingHandler>(
             std::forward<Sink>(sink));
     }
 };
 
-template <class Ch, class Tr, class Allocator>
+template <class Ch, class Tr, class Allocator,
+    class SkippingHandler = fail_if_skipped>
 struct string_field_translator_factory
 {
     using target_type = std::basic_string<Ch, Tr, Allocator>;
 
     template <class Sink>
-    string_field_translator<std::decay_t<Sink>, Ch, Tr, Allocator>
-        operator()(Sink&& sink) const
+    string_field_translator<std::decay_t<Sink>, Ch, Tr, Allocator,
+        SkippingHandler> operator()(Sink&& sink) const
     {
-        return string_field_translator<std::decay_t<Sink>, Ch, Tr, Allocator>(
+        return string_field_translator<std::decay_t<Sink>, Ch, Tr, Allocator, SkippingHandler>(
             std::forward<Sink>(sink));
     }
 };
@@ -102,6 +106,14 @@ struct default_field_translator_factory<T,
     using type = arithmetic_field_translator_factory<T>;
 };
 
+template <class T>
+struct default_field_translator_factory<std::optional<T>,
+    std::enable_if_t<is_default_translatable_arithmetic_type_v<T>>>
+{
+    using type = arithmetic_field_translator_factory<T,
+        ignore_if_skipped, ignore_if_conversion_failed>;
+};
+
 template <class Ch, class Tr, class Allocator>
 struct default_field_translator_factory<
     std::basic_string<Ch, Tr, Allocator>, void>
@@ -109,11 +121,29 @@ struct default_field_translator_factory<
     using type = string_field_translator_factory<Ch, Tr, Allocator>;
 };
 
+template <class Ch, class Tr, class Allocator>
+struct default_field_translator_factory<
+    std::optional<std::basic_string<Ch, Tr, Allocator>>, void>
+{
+    using type = string_field_translator_factory<Ch, Tr, Allocator, ignore_if_skipped>;
+};
+
 template <class Ch, class Tr>
 struct default_field_translator_factory<std::basic_string_view<Ch, Tr>, void>
 {
     using type = string_view_field_translator_factory<Ch, Tr>;
 };
+
+template <class Ch, class Tr>
+struct default_field_translator_factory<std::optional<std::basic_string_view<Ch, Tr>>, void>
+{
+    using type = string_view_field_translator_factory<Ch, Tr, ignore_if_skipped>;
+};
+
+//template <class T>
+//struct default_field_translator_factory<std::optional<T>> :
+//    default_field_translator_factory<T, ignore_if_skipped, ignore_if_conversion_failed>
+//{};
 
 template <class T>
 using default_field_translator_factory_t =
@@ -127,6 +157,7 @@ struct basic_field_spec
     FieldTranslatorFactory factory; // TODO: Employ EBO
 
     using target_type = T;
+    using factory_type = FieldTranslatorFactory;
 
     basic_field_spec(std::basic_string<Ch, Tr, Allocator> text_field_name,
         FieldTranslatorFactory&& field_translator_factory
@@ -181,17 +212,17 @@ basic_field_spec<Ch, std::char_traits<Ch>, std::allocator<Ch>, T,
 
 namespace detail::record_xlate {
 
-template <class FieldTranslatorFactory>
+template <class FieldTranslatorFactory, class Stored>
 class field_scanner_setter :
     detail::member_like_base<FieldTranslatorFactory>
 {
     using target_type = typename FieldTranslatorFactory::target_type;
 
-    std::optional<target_type>* field_value_;
+    Stored* field_value_;
 
 public:
     explicit field_scanner_setter(FieldTranslatorFactory factory,
-        std::optional<target_type>& field_value) :
+        Stored& field_value) :
         detail::member_like_base<FieldTranslatorFactory>(std::move(factory)),
         field_value_(std::addressof(field_value))
     {}
@@ -201,11 +232,30 @@ public:
         scanner.set_field_scanner(field_index, this->get()(
             [field_value = field_value_](auto&& v) {
                 using v_t = decltype(v);
-                static_assert(std::is_same_v<target_type, std::decay_t<v_t>>);
-                field_value->emplace(std::forward<v_t>(v));
+                if constexpr (detail::is_std_optional_v<
+                        std::decay_t<decltype(*field_value)>>) {
+                    field_value->emplace(std::forward<v_t>(v));
+                } else {
+                    std::get<0>(*field_value).emplace(std::forward<v_t>(v));
+                }
             }));
     }
 };
+
+template <class T>
+struct stored
+{
+    using type = std::optional<std::tuple<T>>;
+};
+
+template <class T>
+struct stored<std::optional<T>>
+{
+    using type = std::optional<T>;
+};
+
+template <class T>
+using stored_t = typename stored<T>::type;
 
 template <class... Ts>
 class record_translator_header_field_scanner
@@ -217,7 +267,7 @@ class record_translator_header_field_scanner
 public:
     template <class... FieldSpecRs>
     explicit record_translator_header_field_scanner(
-        std::tuple<std::optional<Ts>...>& field_values,
+        std::tuple<stored_t<Ts>...>& field_values,
         FieldSpecRs&&... specs) :
         record_translator_header_field_scanner(
             field_values,
@@ -228,7 +278,7 @@ public:
 private:
     template <std::size_t I, class FieldSpecR, class... FieldSpecRs>
     record_translator_header_field_scanner(
-        std::tuple<std::optional<Ts>...>& field_values,
+        std::tuple<stored_t<Ts>...>& field_values,
         std::integral_constant<std::size_t, I>,
         FieldSpecR&& spec,
         FieldSpecRs&&... specs) :
@@ -244,11 +294,12 @@ private:
             field_scanner_setter(
                 detail::forward_if<FieldSpecR>(spec.factory),
                 std::get<I>(field_values)));
+                    // std::optional<T> or std::optional<std::tuple<T>>
     }
 
     template <std::size_t I>
     explicit record_translator_header_field_scanner(
-        std::tuple<std::optional<Ts>...>&,
+        std::tuple<detail::record_xlate::stored_t<Ts>...>&,
         std::integral_constant<std::size_t, I>)
     {}
 
@@ -276,7 +327,8 @@ template <class F, class... FieldSpecs>
 class record_translator_record_end_scanner
 {
     using to_t =
-        std::tuple<std::optional<typename FieldSpecs::target_type>...>;
+        std::tuple<detail::record_xlate::stored_t<
+            typename FieldSpecs::target_type>...>;
     using h_t = record_translator_header_field_scanner<
         typename FieldSpecs::target_type...>;
 
@@ -318,11 +370,36 @@ public:
         std::apply(
             f_,
             detail::transform(
-                [](auto& v) { return std::move(*v); },
-                    // TODO: Whether to dereference or not should be decided
-                    // with regard to whether the target type of FieldSpecs_i
-                    // is an instance of std::optional or not
+                [](auto& v) { return bleed(v); },
                 *field_values_));
+        clear_field_values();
+    }
+
+private:
+    template <class T>
+    static T&& bleed(std::optional<std::tuple<T>>& o)
+    {
+        return std::move(std::get<0>(*o));
+    }
+
+    template <class T>
+    static std::optional<T>&& bleed(std::optional<T>& t)
+    {
+        return std::move(t);
+    }
+
+    void clear_field_values()
+    {
+        clear_field_value(std::integral_constant<std::size_t, 0U>());
+    }
+
+    template <std::size_t I>
+    void clear_field_value(std::integral_constant<std::size_t, I>)
+    {
+        if constexpr (I < sizeof...(FieldSpecs)) {
+            std::get<I>(*field_values_).reset();
+            clear_field_value(std::integral_constant<std::size_t, I + 1>());
+        }
     }
 };
 
