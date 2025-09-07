a12d170c1c64d66b838da52abfd5939f0dcb1e4d
furfurylic <17105140+furfurylic@users.noreply.github.com>
2025-09-07 13:11:44 +0900

    Make record translators aware of handlers

--- src_test/TestRecordTranslator.cpp
+++ src_test/TestRecordTranslator.cpp
@@ -35,13 +35,16 @@ TEST_F(TestRecordTranslator, All)
         },
         field_spec<std::string>("Name"),
         field_spec<std::optional<std::size_t>>("#"sv),
-        field_spec<double>("Mass"s));
+        field_spec<double>("Mass"s,
+            arithmetic_field_translator_factory<
+                double, replace_if_skipped<double>>(-1.0)));
     parse_tsv(make_char_input(
         "#\tName\tMass\n"
         "3\tEarth\t1\n"
         "2\tVenus\t0.815\n"
         "4\tMars\t0.107\n"
-        "\tEris\t0.0002757\n"),
+        "\tEris\t0.0002757\n"
+        "\tSedna\n"),
         std::move(t));
 
     std::vector<planet_t> expected;
@@ -49,5 +52,6 @@ TEST_F(TestRecordTranslator, All)
     expected.emplace_back(2, "Venus", std::stod("0.815"));
     expected.emplace_back(4, "Mars", std::stod("0.107"));
     expected.emplace_back(0, "Eris", std::stod("0.0002757"));
+    expected.emplace_back(0, "Sedna", -1.0);
     ASSERT_EQ(expected, planets);
 }
