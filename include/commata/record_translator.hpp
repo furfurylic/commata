98d964452fabcdf38dd79e813ef0b9006f4ac07a
furfurylic <17105140+furfurylic@users.noreply.github.com>
2026-05-20 19:18:05 +0900

    Remove a default template param of make_basic_record_translator_impl

--- include/commata/record_translator.hpp
+++ include/commata/record_translator.hpp
@@ -584,7 +584,7 @@ private:
     }
 };
 
-template <class Ch, class Tr = std::char_traits<Ch>, bool UsesAllocatorForPred,
+template <class Ch, class Tr, bool UsesAllocatorForPred,
           class Allocator, class FR, class... FieldSpecRs>
 [[nodiscard]] basic_table_scanner<Ch, Tr, Allocator>
     make_basic_record_translator_impl(
